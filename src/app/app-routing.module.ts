1a70c5442725859ae76915d182f8119407f1ea92
anaviktoriv <anaviktoriv@gmail.com>
2021-05-01 17:27:23 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { SparePartComponent } from './spare-part/spare-part.component';
+import { SparePartsComponent } from './spare-parts/spare-parts.component';
+import { TipsComponent } from './tips/tips.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'spareparts', component: SparePartsComponent },
+  { path: 'spareparts/:id', component: SparePartComponent },
+  { path: 'tips', component: TipsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { SparePartsComponent } from './spare-parts/spare-parts.component';
+import { TipsComponent } from './tips/tips.component';
+import { SparePartComponent } from './spare-part/spare-part.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomeComponent,
+    ServicesComponent,
+    SparePartsComponent,
+    TipsComponent,
+    SparePartComponent
   ],
   imports: [
     BrowserModule,
