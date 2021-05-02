92baafc7987ed7e7b008bad392792afbb1f05d07
anaviktoriv <anaviktoriv@gmail.com>
2021-05-02 17:30:45 +0200

    add search bar input animation and toggle buttons

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -7,7 +7,28 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
+  @ViewChild('search') searchElement: ElementRef;
+  searchBarIsOpen = false;
   constructor(public translate: TranslateService) {}
 
   ngOnInit(): void {}
+
+  onClickSearchIcon() {
+    this.searchBarIsOpen = true;
+    setTimeout(() => {
+      this.searchElement.nativeElement.focus();
+    }, 0);
+  }
+  onClickCloseIcon() {
+    this.searchBarIsOpen = false;
+    setTimeout(() => {
+      this.searchElement.nativeElement.value = '';
+    }, 0);
+  }
+  toggleSearchIconDisplay() {
+    return this.searchBarIsOpen ? 'none' : 'block';
+  }
+  toggleCloseIconDisplay() {
+    return this.searchBarIsOpen ? 'block' : 'none';
+  }
 }
