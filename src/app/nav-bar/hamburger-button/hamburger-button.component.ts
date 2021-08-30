a539e1e84ec87b12366edf50905af556564825f6
anaviktoriv <anaviktoriv@gmail.com>
2021-08-30 08:51:00 +0200

    add mobile 100vh height for menu and minor css refactor

--- src/app/nav-bar/hamburger-button/hamburger-button.component.ts
+++ src/app/nav-bar/hamburger-button/hamburger-button.component.ts
@@ -12,6 +12,7 @@ export class HamburgerButtonComponent implements OnInit {
   constructor() {}
 
   public onClickHandler() {
+    console.log('menu is button clicked');
     this.menuIsOpenChange.emit(!this.menuIsOpen);
   }
 
